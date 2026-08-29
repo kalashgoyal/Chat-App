16cf932a569a0038a054984f706d4f7c4d53e925
kalashgoyal <kalashgoyal07@gmail.com>
2026-08-29 14:22:58 +0530

    Connect frontend to production backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE==="development" ? "http://localhost:8000/api":"/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:8000/api"
+      : "https://chat-app-5ap2.onrender.com/api",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "https://chat-app-5ap2.onrender.com";
 
 export const useAuthStore = create((set,get) => ({
 
