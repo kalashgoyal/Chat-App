d90330dac55c7243a0c048a537db7d12fd6f28a1
kalashgoyal <kalashgoyal07@gmail.com>
2026-08-29 15:40:01 +0530

    Fix production API URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000/api"
-      : "https://chat-app-ymmq.onrender.com",
+      : "https://chat-app-ymmq.onrender.com/api",
   withCredentials: true,
 });
